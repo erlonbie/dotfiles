0d9c07606d231da5f7a30ac1063c5be49296a00e
erlonbie <erlonminievix@gmail.com>
2021-12-28 00:51:13 -0400

    script updated

--- folder_sync.sh
+++ folder_sync.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
 
-cp -r ~/.config/qtile ~/.config/dotfiles/qtile
-cp -r ~/.config/ranger ~/.config/dotfiles/ranger
+ln -s ~/.config/dotfiles/.bashrc ~/.bashrc
+ln -s ~/.config/dotfiles/.alacritty.yml ~/.alacritty.yml
+ln -s ~/.config/dotfiles/.vimrc ~/.vimrc
+ln -s ~/.config/dotfiles/.tmux.conf ~/.tmux.conf
+ln -s ~/.config/dotfiles/coc-settings.json ~/.vim/coc-settings.json
+
+ln -s ~/.config/dotfiles/qtile/* ~/.config/qtile/
+ln -s ~/.config/dotfiles/ranger/* ~/.config/ranger/
