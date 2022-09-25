ce9ded9d141c1e043ec72c28bde54ba1c00560b2
erlonbie <erlonminievix@gmail.com>
2022-09-25 12:37:03 -0400

    hyprland waybar wlogout

---
+++ hypr/autostart.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# start-pulseaudio-x11 &
+# pipewire &
+# pipewire-pulse &
+# wireplumber &
+nm-applet --indicator &
+copyq &
+fcitx5 &
+# setxkbmap -layout br,us
+# setxkbmap -option 'grp:alt_shift_toggle'
